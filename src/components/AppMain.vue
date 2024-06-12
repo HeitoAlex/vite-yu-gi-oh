e49e873082a11c0cd97f7a347ce868fde5f110c5
HeitoAlex <giallorossotuttalavita@hotmail.it>
2024-06-12 19:49:19 +0200

    creati tutti i fogli necessari al progetto e importato bootstrap e axios

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,11 +1,25 @@
 <script>
+import AppSearch from './AppSearch.vue';
+import CardList from './CardList.vue';
 
+export default {
+    data(){
+        return{
+            
+        }
+    },
+    components:{
+        AppSearch,
+        CardList
+    }
+}
 </script>
 
 <template>
-
+    <AppSearch/>
+    <CardList/>
 </template>
 
-<style scoped>
+<style lang="scss" scoped>
 
 </style>
---
+++ src/components/CardList.vue
@@ -0,0 +1,22 @@
+<script>
+import SingleCard from './SingleCard.vue';
+
+export default {
+    data(){
+        return{
+            
+        }
+    },
+    components:{
+        SingleCard
+    }
+}
+</script>
+
+<template>
+    <SingleCard/>
+</template>
+
+<style scoped>
+
+</style>
