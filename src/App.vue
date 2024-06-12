5c3d8ceb37ef8bcb2e00dc66b9bf0be14bd3e7ae
HeitoAlex <giallorossotuttalavita@hotmail.it>
2024-06-12 19:00:49 +0200

    scaffolding

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script>
+
+export default{
+  data(){
+    return{
+
+    }
+  }
+}
+</script>
+
+<template>
+
+</template>
+
+<style lang="scss">
+  @use './styles/partials/mixins';
+  @use './styles/partials/variables';
+  @use './styles/general.scss';
+  @use '../node_modules/bootstrap/scss/bootstrap.scss';
+
+</style>
